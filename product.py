dceeb8fca1eb84be92c29e5bff7f45ed689e7c4d
PoYiResearch <poyichenresearch@gmail.com>
2026-01-20 09:42:50 +0800

    read in

--- product.py
+++ product.py
@@ -1,3 +1,15 @@
+## read in file
+
+product = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name,price = line.strip().split(',')
+		product.append([name,price])
+print(product)
+
+## user enter:
 products = []
 while True:
 	name = input('pls enter the name of the product: ')
@@ -5,8 +17,9 @@ while True:
 		break
 	price = input('enter the price of this product: ')
 	products.append([name,price])
-#print(products)
+print(products)
 
+# print out results
 for p in products:
 	print(p)
 
